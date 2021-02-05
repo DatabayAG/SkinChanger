cd575fc2e8f60c32e8745a8fcacd2b477037becc
Marvin Beym <mBeym@databay.de>
2021-02-05 08:49:39 +0100

    Replaced css !important with higher score. Replaced jquery with native javascript.

--- src/Form/Input/SelectAllocationInput/selectAllocation_input.js
+++ src/Form/Input/SelectAllocationInput/selectAllocation_input.js
@@ -1,20 +1,38 @@
-$( document ).ready(() => {
-  $("button[name=add]").on("click", addCell);
-  $("button[name=remove]").on("click", removeCell)
-});
+document.addEventListener("DOMContentLoaded", function (event) {
 
-function addCell(){
-  let cell = $(this).parent().parent();
-  let clone = cell.clone().insertAfter(cell);
-  clone.find("button[name=add]").on("click", addCell);
-  clone.find("button[name=remove]").on("click", removeCell);
-}
-
-function removeCell(){
-  let cell = $(this).parent().parent();
-  let cells = cell.parent().find(".selectAllocation_cell");
-
-  if(cells.length > 1){
-    cell.remove();
+  /**
+   * Initializes the existing buttons on page load.
+   */
+  const init = (inputClass = '.selectAllocation_input') => {
+    document.querySelectorAll(inputClass + ' .selectAllocation_row').forEach(element => {
+      element.querySelector("button[name=add]").addEventListener("click", addRow);
+      element.querySelector("button[name=remove]").addEventListener("click", removeRow);
+    })
   }
-}
+
+  /**
+   * Adds a new row below the previous one.
+   * @param event
+   */
+  const addRow = (event) => {
+    let row = event.target.parentNode.parentNode;
+    let clone = row.cloneNode(true);
+    row.after(clone);
+    clone.querySelector("button[name=add]").addEventListener("click", addRow);
+    clone.querySelector("button[name=remove]").addEventListener("click", removeRow);
+  }
+
+  /**
+   * Removes the row
+   * @param event
+   */
+  const removeRow = (event) => {
+    let row = event.target.parentNode.parentNode;
+    let rows = row.parentNode.querySelectorAll(".selectAllocation_row");
+    if(rows.length > 1){
+      row.remove();
+    }
+  }
+
+  init();
+});
